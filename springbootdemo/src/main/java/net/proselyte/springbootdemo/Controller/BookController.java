c4e7b4cba8da11e473462dd8d12f15e30842ff34
Anne <annilinum@mail.ru>
2023-01-02 16:09:26 +0300

    method create book, red methog fieldFreeBooks

--- springbootdemo/src/main/java/net/proselyte/springbootdemo/Controller/BookController.java
+++ springbootdemo/src/main/java/net/proselyte/springbootdemo/Controller/BookController.java
@@ -1,5 +1,6 @@
 package net.proselyte.springbootdemo.Controller;
 
+import javax.validation.Valid;
 import net.proselyte.springbootdemo.Model.Book;
 import net.proselyte.springbootdemo.Service.BookService;
 import org.springframework.data.domain.Page;
@@ -7,86 +8,84 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
-    private final BookService bookService;
-    private final PageableHelper pageableHelper;
+  private final BookService bookService;
+  private final PageableHelper pageableHelper;
 
-    public BookController(BookService bookService, PageableHelper pageableHelper) {
-        this.bookService = bookService;
-        this.pageableHelper = pageableHelper;
+  public BookController(BookService bookService, PageableHelper pageableHelper) {
+    this.bookService = bookService;
+    this.pageableHelper = pageableHelper;
+  }
+
+  @GetMapping("/books")
+  public String findAllBooks(Model model,
+      @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Integer pageNumber,
+      @RequestParam(value = "sortField", required = false, defaultValue = "title") String sortField,
+      @RequestParam(value = "sortType", required = false, defaultValue = "ASC") String sortType) {
+
+    Page<Book> page = bookService.findAll(pageNumber, sortField, Sort.Direction.fromString(sortType));
+    model.addAttribute("books", page.toList());
+    pageableHelper.fillPageable(model, pageNumber, page.getTotalPages(), sortField, sortType);
+    return "/books";
+  }
+
+  @GetMapping("/books/{bookId}")
+  public String deleteBook(@PathVariable("bookId") Long bookId) {
+    bookService.deleteBook(bookId);
+    return "redirect:/books";
+  }
+
+  @GetMapping("/book-delete/{userId}/{id}")
+  public String deleteUserFromBook(@PathVariable("id") Long bookId, @PathVariable("userId") Long userId) {
+    bookService.deleteUserFromBook(bookId, userId);
+    return "redirect:/user-books/" + userId;
+  }
+
+  @PostMapping("/issue-book")
+  public String issueBook(Long bookId, Long userId) {
+    bookService.issueBook(bookId, userId);
+    return "redirect:/user-books/" + userId;
+  }
+
+  @GetMapping("/create-new-book")
+  public String createBookForm(@ModelAttribute(name = "newBook") CreateBookRequest newBook) {
+    return "book-create";
+  }
+
+  @PostMapping("/create-new-book")
+  public String createBook(@Valid @ModelAttribute(name = "newBook") CreateBookRequest newBook, Errors errors) {
+    if (errors.hasErrors()) {
+      return "book-create";
     }
+    bookService.createNewBook(newBook.getTitle(), newBook.getAuthor(), newBook.getCountBook());
+    return "redirect:/books";
+  }
 
-    @GetMapping("/books")
-    public String findAllBooks(Model model, @ModelAttribute("newBook") CreateBookRequest newBook,
-                               @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Integer pageNumber,
-                               @RequestParam(value = "sortField", required = false, defaultValue = "title") String sortField,
-                               @RequestParam(value = "sortType", required = false, defaultValue = "ASC") String sortType) {
+  @GetMapping("/book-update/{id}")
+  public String updateBookForm(@PathVariable("id") Long id, Model model) {
+    Book book = bookService.findById(id);
+    model.addAttribute("book", book);
+    return "/book-update";
+  }
 
-        if (model.getAttribute("newBookFormErrors") != null) {
-            model.addAttribute("org.springframework.validation.BindingResult.newBook",
-                    model.getAttribute("newBookFormErrors"));
-        }
+  @PostMapping("/book-update")
+  public String updateBook(@Valid Book book, Errors errors) {
+    if (errors.hasErrors()) return "/book-update";
+    bookService.saveBook(book);
+    return "redirect:/books";
+  }
 
-        Page<Book> page = bookService.findAll(pageNumber, sortField, Sort.Direction.fromString(sortType));
-        model.addAttribute("books", page.toList());
-        pageableHelper.fillPageable(model, pageNumber, page.getTotalPages(), sortField, sortType);
-        return "/books";
-    }
-
-    @GetMapping("/books/{bookId}")
-    public String deleteBook(@PathVariable("bookId") Long bookId) {
-        bookService.deleteBook(bookId);
-        return "redirect:/books";
-    }
-
-    @GetMapping("/book-delete/{userId}/{id}")
-    public String deleteUserFromBook(@PathVariable("id") Long bookId, @PathVariable("userId") Long userId) {
-        bookService.deleteUserFromBook(bookId, userId);
-        return "redirect:/user-books/" + userId;
-    }
-
-    @PostMapping("/create-book")
-    public String createBook(Long bookId, Long userId) {
-        bookService.saveBook(bookId, userId);
-        return "redirect:/user-books/" + userId;
-    }
-
-    @PostMapping("/create-newBook")
-    public String createBook(@Valid CreateBookRequest newBook, Errors errors, RedirectAttributes redirectAttributes) {
-        if (errors.hasErrors()) {
-            redirectAttributes.addFlashAttribute("newBookFormErrors", errors);
-            redirectAttributes.addFlashAttribute("newBook", newBook);
-            return "redirect:/books";
-        }
-        bookService.createNewBook(newBook.getTitle(), newBook.getAuthor());
-        return "redirect:/books";
-    }
-
-    @GetMapping("/book-update/{id}")
-    public String updateBookForm(@PathVariable("id") Long id, Model model) {
-        Book book = bookService.findById(id);
-        model.addAttribute("book", book);
-        return "/book-update";
-    }
-
-    @PostMapping("/book-update")
-    public String updateBook(@Valid Book book, Errors errors) {
-        if (errors.hasErrors())
-            return "/book-update";
-        bookService.saveBook(book);
-        return "redirect:/books";
-    }
-
-    @GetMapping("/readers/{id}")
-    public String getReader(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("readers", bookService.getReaderByBookId(id));
-        model.addAttribute("bookId", id);
-        return "readers";
-    }
+  @GetMapping("/readers/{id}")
+  public String getReader(@PathVariable("id") Long id, Model model) {
+    model.addAttribute("readers", bookService.getReaderByBookId(id));
+    model.addAttribute("bookId", id);
+    return "readers";
+  }
 }
--- springbootdemo/src/main/java/net/proselyte/springbootdemo/Controller/CreateBookRequest.java
+++ springbootdemo/src/main/java/net/proselyte/springbootdemo/Controller/CreateBookRequest.java
@@ -1,12 +1,23 @@
 package net.proselyte.springbootdemo.Controller;
 
-import lombok.Data;
-
 import javax.validation.constraints.NotEmpty;
-@Data
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateBookRequest {
-    @NotEmpty(message = "Поле не может быть пустым")
-    private String author;
-    @NotEmpty(message = "Поле не может быть пустым")
-    private String title;
+  @NotEmpty(message = "Поле не может быть пустым")
+  private String author;
+  @NotEmpty(message = "Поле не может быть пустым")
+  private String title;
+
+  private int countBook;
+
+  @Override public String toString() {
+    return "CreateBookRequest{" +
+        "author='" + author + '\'' +
+        ", title='" + title + '\'' +
+        '}';
+  }
 }
