66592f2dc75e9754f65fc95df5c814604fe7bb36
Anne <annilinum@mail.ru>
2023-01-08 15:27:42 +0300

    fix gitignore

--- springbootdemo/src/main/java/net/proselyte/springbootdemo/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package net.proselyte.springbootdemo.Repository;
-
-import net.proselyte.springbootdemo.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
